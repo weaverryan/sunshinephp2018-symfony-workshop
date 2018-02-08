38723e11e15f74226039dddaaf11fb5065e95a2f
Ryan Weaver <ryan@thatsquality.com>
2018-02-08 06:18:00 -0500

    Step 22: Subscriber: log user agent

--- src/EventSubscriber/UserAgentLoggerSubscriber.php
+++ src/EventSubscriber/UserAgentLoggerSubscriber.php
@@ -2,15 +2,23 @@
 
 namespace App\EventSubscriber;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class UserAgentLoggerSubscriber implements EventSubscriberInterface
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
         $userAgent = $event->getRequest()->headers->get('User-Agent');
-        dump($userAgent);die;
+        $this->logger->info('The user agent is: '.$userAgent);
     }
 
     public static function getSubscribedEvents()
